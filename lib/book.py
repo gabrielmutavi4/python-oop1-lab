8f70235c1c12be6674b95e6f73afe97e28f0450c
gabrielmutavi4-GM <gabrielmutavi4@gmail.com>
2026-06-07 12:41:35 +0300

    initial commit of project files

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        # store title
+        self.title = title
+
+        # validate page_count
+        if isinstance(page_count, int):
+            self.page_count = page_count
+        else:
+            print("page_count must be an integer")
+            self.page_count = 0  
+
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
