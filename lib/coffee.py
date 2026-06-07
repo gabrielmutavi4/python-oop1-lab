45cc843860874694506510a70760d42b6e07f142
gabrielmutavi4-GM <gabrielmutavi4@gmail.com>
2026-06-07 12:48:06 +0300

    update project files

--- lib/coffee.py
+++ lib/coffee.py
@@ -2,16 +2,20 @@
 
 class Coffee:
     def __init__(self, size, price):
-        # validate size
-        if size in ["Small", "Medium", "Large"]:
-            self.size = size
-        else:
-            print("size must be Small, Medium, or Large")
-            self.size = "Small"  
-
+        self.size = size
         self.price = price
 
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
 
     def tip(self):
-        print("This coffee is great, here's a tip!")
+        print("This coffee is great, here’s a tip!")
         self.price += 1
